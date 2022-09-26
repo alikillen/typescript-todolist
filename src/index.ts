d21e0276eb752e2ef8a22aa3f099bbcce3a609e9
Alison Killen <61318985+alikillen@users.noreply.github.com>
2022-09-26 18:22:18 +1000

    basic functionality working, need to add local storage capability

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,9 @@ const list = document.querySelector<HTMLUListElement>("#list")
 const form = document.querySelector("#new-task-form") as HTMLFormElement | null
 const input = document.querySelector<HTMLInputElement>("#new-task-title")
 
+// creating an array of type Task to store in local memory
+const tasks: Task[] = []
+
 form?.addEventListener("submit", e=> {
   e.preventDefault()
 
@@ -25,14 +28,22 @@ form?.addEventListener("submit", e=> {
   if (input?.value == "" || input?.value == null) return
 
   // typescript will know that newTask is a type of Task since it has all the same properties
-  const newTask = {
+  // but its nice to declare specific types for your variables if you have them
+  const newTask: Task = {
     id: uuidV4(),
     title: input.value,
     completed: false,
     createdAt: new Date()
   }
 
+  // pushing new task into overall task list to save in local storage
+  tasks.push(newTask)
+
+  // when we hover over addListItem we can see what we expect to be returned from this function (void if it is not returning anything)
   addListItem(newTask)
+
+  // when we press enter it will set input back to empty string
+  input.value = ""
 
 }) 
 
@@ -41,4 +52,21 @@ const myInput = input?.value
 console.log("myInput =", myInput)
 
 // declaring our task variable as type task which we initiated at beginning of file
-function addListItem(task: Task) {}
+// we can also say addListItem(task: Task): boolean, which would define that we are returning a boolean. if we then tried to return a string/int, it would error. smart!
+function addListItem(task: Task) {
+  const item = document.createElement("li")
+  // typescript knows createElement is an li type since we passed one in. handy!
+
+  const label = document.createElement("label")
+  const checkbox = document.createElement("input")
+  checkbox.addEventListener("change", () => {
+    task.completed = checkbox.checked
+    console.log(tasks)
+  })
+  checkbox.type = "checkbox"
+  checkbox.checked = task.completed
+
+  label.append(checkbox, task.title)
+  item.append(label)
+  list?.append(item)
+}
